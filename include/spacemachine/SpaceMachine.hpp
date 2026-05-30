97426c1534c672a22d390aad666ff5e934624783
LuNoX <maillunox@gmail.com>
2026-05-30 05:32:26 +0200

    Improved error messages for static asserts

--- include/spacemachine/SpaceMachine.hpp
+++ include/spacemachine/SpaceMachine.hpp
@@ -103,50 +103,58 @@ struct is_self_constructing<Self, T>
 template<typename Self, typename T>
 constexpr bool is_self_constructing_v = is_self_constructing<Self, T>::value;
 
-template<typename TargetID, typename... States>
-struct contains_state_with_id
-    : polyfill::integral_constant<bool,
-                                  polyfill::disjunction_v<polyfill::is_same<
-                                          typename States::ID, TargetID>...>> {
+template<typename T, typename... Ts>
+struct is_in_pack
+    : polyfill::integral_constant<
+              bool, polyfill::disjunction_v<polyfill::is_same<T, Ts>...>> {};
+
+template<typename T, typename... Ts>
+constexpr bool is_in_pack_v = is_in_pack<T, Ts...>::value;
+
+template<typename T, typename... Ts>
+struct is_not_in_pack
+    : polyfill::integral_constant<bool, !is_in_pack_v<T, Ts...>> {};
+
+template<typename T, typename... Ts>
+constexpr bool is_not_in_pack_v = is_not_in_pack<T, Ts...>::value;
+
+template<typename... StateIDs>
+struct all_state_ids_unique : polyfill::true_type {};
+
+template<typename Head, typename... Tail>
+struct all_state_ids_unique<Head, Tail...>
+    : polyfill::integral_constant<
+              bool, is_not_in_pack_v<Head, Tail...>
+                            && all_state_ids_unique<Tail...>::value> {
+    static_assert(
+            is_not_in_pack_v<Head, Tail...>,
+            "Duplicate state ID detected! See backtrace for offending ID.");
 };
 
 template<typename TargetID, typename... States>
-constexpr bool contains_state_with_id_v
-        = contains_state_with_id<TargetID, States...>::value;
+struct is_known_state_id : is_in_pack<TargetID, typename States::ID...> {};
 
-template<typename... IDs>
-struct contains_no_duplicates : polyfill::true_type {};
+template<typename TargetID, typename... States>
+constexpr bool is_known_state_id_v
+        = is_known_state_id<TargetID, States...>::value;
 
-template<typename Head, typename... Tail>
-struct contains_no_duplicates<Head, Tail...>
-    : polyfill::integral_constant<
-              bool, !polyfill::disjunction_v<polyfill::is_same<Head, Tail>...>
-                            && contains_no_duplicates<Tail...>::value> {};
-
-template<typename... States>
-constexpr bool are_state_ids_unique_v
-        = contains_no_duplicates<typename States::ID...>::value;
-
-template<typename State, typename... AllStates>
-struct are_transition_targets_known;
+template<typename State, typename... States>
+struct transitions_target_known_ids;
 
 template<typename StateID, typename Fn, typename... Transitions,
-         typename... AllStates>
-struct are_transition_targets_known<
-        SpaceMachine::State<StateID, Fn, Transitions...>, AllStates...>
+         typename... States>
+struct transitions_target_known_ids<
+        SpaceMachine::State<StateID, Fn, Transitions...>, States...>
     : polyfill::integral_constant<
-              bool, polyfill::conjunction_v<contains_state_with_id<
-                            typename Transitions::To, AllStates...>...>> {};
+              bool, polyfill::conjunction_v<is_known_state_id<
+                            typename Transitions::To, States...>...>> {};
 
 template<typename... States>
-struct is_every_transition_target_known
-    : polyfill::integral_constant<
-              bool, polyfill::conjunction_v<are_transition_targets_known<
-                            States, States...>...>> {};
+struct transitions_target_registered_states {
+    template<typename State>
+    using type = transitions_target_known_ids<State, States...>;
+};
 
-template<typename... States>
-constexpr bool is_every_transition_target_known_v
-        = is_every_transition_target_known<States...>::value;
 } // namespace traits
 
 namespace detail {
@@ -292,8 +300,23 @@ struct FindStateIndexImpl<TargetID, Index> {
     static_assert(always_false_v<TargetID>,
                   "State with given target ID not in provided TypeList!");
 };
-
 } // namespace detail
+
+template<template<typename> class Trait, typename... Types>
+struct for_each_type;
+
+template<template<typename> class Trait>
+struct for_each_type<Trait> : polyfill::true_type {};
+
+template<template<typename> class Trait, typename Head, typename... Tail>
+struct for_each_type<Trait, Head, Tail...>
+    : polyfill::integral_constant<
+              bool,
+              Trait<Head>::value && for_each_type<Trait, Tail...>::value> {
+    static_assert(Trait<Head>::value,
+                  "Check failed on type! See template instantiation backtrace "
+                  "for offending type.");
+};
 
 template<typename... Types>
 struct TypeList
@@ -421,14 +444,16 @@ auto MakeState(Fn&& /*work*/, Transitions&&... /*transitions*/)
 /// declare higher-priority transitions earlier.
 template<typename InitialStateID, typename... States>
 struct StateMachine {
-    // TODO: make these static asserts more user-friendly by printing the
-    // offending state IDs or transition targets.
-    static_assert(traits::contains_state_with_id_v<InitialStateID, States...>,
+    static_assert(traits::is_known_state_id_v<InitialStateID, States...>,
                   "Initial state ID must be one of the registered states!");
-    static_assert(traits::are_state_ids_unique_v<States...>,
-                  "State IDs must be unique!");
-    static_assert(traits::is_every_transition_target_known_v<States...>,
-                  "All transition targets must be registered states!");
+    static_assert(traits::all_state_ids_unique<typename States::ID...>::value,
+                  "Duplicate state ID! See backtrace for offending ID.");
+    static_assert(
+            for_each_type<traits::transitions_target_registered_states<
+                                  States...>::template type,
+                          States...>::value,
+            "Transition targets unknown state ID! See backtrace for offending "
+            "state.");
 
     TypeList<States...> states;
     polyfill::size_t activeStateIndex;
